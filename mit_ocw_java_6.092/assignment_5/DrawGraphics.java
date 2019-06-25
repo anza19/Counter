07846c02d5d74dd63847ff3c041dc596b68a7303
anza19 <anza.k.9539@gmail.com>
2019-06-25 16:27:35 -0400

    Testing drawing different shapes.

--- mit_ocw_java_6.092/assignment_5/DrawGraphics.java
+++ mit_ocw_java_6.092/assignment_5/DrawGraphics.java
@@ -7,22 +7,22 @@ public class DrawGraphics {
     
     /** Initializes this class for drawing. */
     public DrawGraphics() {
-        box = new BouncingBox(200, 50, Color.RED);
+        box = new BouncingBox(200, 200, Color.MAGENTA);
     }
 
     /** Draw the contents of the window on surface. Called 20 times per second. */
     public void draw(Graphics surface) {
-        surface.drawLine(50, 50, 250, 250);
-        surface.drawOval(60, 40, 200, 300);
-        int[]x = new int[3];
-        int[]y = new int[3];
-        int n;  // count of points
-        // Make a triangle
-        x[0]=100; x[1]=150; x[2]=50;
-        y[0]=100; y[1]=150; y[2]=150;
-        n = 3;
-        Polygon myTri = new Polygon(x, y, n);  // a triangle
-        surface.drawPolygon(myTri);
+
+        //drawing the box on the window
         box.draw(surface);
+
+        //drawing random shapes on the window
+        surface.drawLine(50, 50, 200, 50);
+
+        //attempting to draw a triangle using drawLine()
+        surface.drawLine(50, 50, 100, 50);
+        surface.drawLine(50, 50, 75, 100);
+        surface.drawLine(75, 100, 100, 50);
+
     }
 } 
