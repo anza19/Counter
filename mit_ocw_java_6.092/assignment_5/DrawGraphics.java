c8eeb986c8f44b2690020fa4f7baa56cf1151f89
anza19 <anza.k.9539@gmail.com>
2019-06-26 10:54:58 -0400

    Drawing a polygon.

--- mit_ocw_java_6.092/assignment_5/DrawGraphics.java
+++ mit_ocw_java_6.092/assignment_5/DrawGraphics.java
@@ -16,13 +16,29 @@ public class DrawGraphics {
         //drawing the box on the window
         box.draw(surface);
 
-        //drawing random shapes on the window
+        //drawing random shapes on the window6;
         surface.drawLine(50, 50, 200, 50);
 
         //attempting to draw a triangle using drawLine()
-        surface.drawLine(50, 50, 100, 50);
-        surface.drawLine(50, 50, 75, 100);
-        surface.drawLine(75, 100, 100, 50);
+        // surface.drawLine(50, 50, 100, 50);
+        // surface.drawLine(50, 50, 75, 100);
+        // surface.drawLine(75, 100, 100, 50);
+
+        //drawing a polygon
+
+        //this is an array of x-coordinates
+        int[] xpoints = {50,100,125,100,50,25};
+
+        //this is an array for y-coordinates
+        int[] ypoints = {100,100,75,50,50,75};
+
+        //number of point
+        int points = xpoints.length;
+
+        //drawing the polygon itself
+        Polygon p = new Polygon(xpoints, ypoints, points);
+        surface.drawPolygon(p);
+        surface.fillPolygon(p);
 
     }
 } 
